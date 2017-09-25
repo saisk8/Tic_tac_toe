6e29a1183da64a94875d5b2c08221dd170c156ad
Sai Somanath.K <k.saisomanath@gmail.com>
2017-09-25 19:35:20 +0530

    Add minimax algorithm

--- js/app.js
+++ js/app.js
@@ -71,7 +71,7 @@ function declareWinner(who) {
 }
 
 function bestSpot() {
-	return emptySquares()[0];
+	return minimax(origBoard, ai).index;
 }
 
 function emptySquares() {
@@ -88,4 +88,55 @@ function checkTie() {
 		return true;
 	}
 	return false;
+}
+
+function minimax(newBoard, player) {
+	var availSpots = emptySquares(newBoard);
+
+	if (checkWin(newBoard, human)) {
+		return {score: -10};
+	} else if (checkWin(newBoard, ai)) {
+		return {score: 10};
+	} else if (availSpots.length === 0) {
+		return {score: 0};
+	}
+	var moves = [];
+	for (var i = 0; i < availSpots.length; i++) {
+		var move = {};
+		move.index = newBoard[availSpots[i]];
+		newBoard[availSpots[i]] = player;
+
+		if (player == ai) {
+			var result = minimax(newBoard, human);
+			move.score = result.score;
+		} else {
+			var result = minimax(newBoard, ai);
+			move.score = result.score;
+		}
+
+		newBoard[availSpots[i]] = move.index;
+
+		moves.push(move);
+	}
+
+	var bestMove;
+	if(player === ai) {
+		var bestScore = -10000;
+		for(var i = 0; i < moves.length; i++) {
+			if (moves[i].score > bestScore) {
+				bestScore = moves[i].score;
+				bestMove = i;
+			}
+		}
+	} else {
+		var bestScore = 10000;
+		for(var i = 0; i < moves.length; i++) {
+			if (moves[i].score < bestScore) {
+				bestScore = moves[i].score;
+				bestMove = i;
+			}
+		}
+	}
+
+	return moves[bestMove];
 }
